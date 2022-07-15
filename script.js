83d41844a53b99a3bcbdf4606df87eeadf846f1c
Anastasiia Novosuolova <anastasiia.novosuolova@gmail.com>
2022-07-15 19:30:49 +0300

    finish desktop version

--- script.js
+++ script.js
@@ -6,21 +6,55 @@ $(document).ready(function () {
     autoplay: true,
     infinite: true,
     initialSlide: 1,
+    slidesToScroll: 1,
     speed: 1000,
-    autoplaySpeed: 1000,
+    autoplaySpeed: 2000,
     pauseOnHover: true,
     pauseOnFocus: true,
     pauseOnDotsHover: true,
     centerMode:true,
+    centerPadding: '0px',
     responsive: [
       {
-        breakpoint: 768,
+        breakpoint: 1080,
         settings: {
           slidesToShow: 2
         }
       },
       {
-        breakpoint: 550,
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+});
+
+$(document).ready(function () {
+  $('.reviews').slick({
+    arrows: true,
+    dots: false,
+    slidesToShow: 3,
+    autoplay: true,
+    infinite: true,
+    initialSlide: 1,
+    slidesToScroll: 1,
+    speed: 1000,
+    autoplaySpeed: 2000,
+    pauseOnHover: true,
+    pauseOnFocus: true,
+    pauseOnDotsHover: true,
+    centerMode:false,
+    responsive: [
+      {
+        breakpoint: 1270,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 700,
         settings: {
           slidesToShow: 1
         }
